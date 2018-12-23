163df65efadf851cc1a95081196bec8a734bea85
Oleg Belov-Andreev <digituser1@gmail.com>
2018-12-23 15:32:38 +0300

    addTypes

---
+++ Types/Units/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+abstract class Unit
+{
+    public string Title;
+    public string ShortTitle;
+    public string TitleParrental;
+    public string ShortTitleParrental;
+
+    protected Unit(string title, string shortTitle, string titleParrental, string shortTitleParrental)
+    {
+        Title = title;
+        ShortTitle = shortTitle;
+        TitleParrental = titleParrental;
+        ShortTitleParrental = shortTitleParrental;
+    }
+}
